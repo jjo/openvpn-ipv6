7cdcf5725eb5b27fd4369337ab33265fd33d531d
alon <alon@e7ae566f-a301-0410-adde-c780ea21d3b5>
2008-05-11 08:27:42 +0000

    More cleanups
    Put platform sdk samples in svn, remove --enable-win32-service and
    --with-win32-sdk-service-sample from autoconf.
    
    Reorder Makefile.am.
    
    Support "make maintainer-clean" to return to svn clean state.
    
    Fixup tap-win32/common.h to include config.h without path if HAVE_CONFIG_H.
    
    Remove gentoo stuff as it is too old and not needed or used by build.
    
    
    
    git-svn-id: http://svn.openvpn.net/projects/openvpn/test/auto/openvpn@2954 e7ae566f-a301-0410-adde-c780ea21d3b5

--- tap-win32/common.h
+++ tap-win32/common.h
@@ -32,10 +32,14 @@
 // common to both.
 //===============================================
 
+#if defined(HAVE_CONFIG_H)
+#include "config.h"
+#else
 #if defined(_MSC_VER) && !defined(TAP_DRIVER_MAJOR_VERSION)
 #include "config-win32.h"
 #else
 #include "../config.h"
+#endif
 #endif
 
 //=============
